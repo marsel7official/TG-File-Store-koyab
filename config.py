b64fc2a5b959932225d3ba0bf508d67064875c3f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-09-24 11:10:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
+APP_ID = int(os.environ.get("APP_ID", "13675555"))
+API_HASH = os.environ.get("API_HASH", "c0da9c346d2c45dbc7ec49a05da9b2b6")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", 5696982423:AAE45yJTxnjhApGbtl1eZXwHSCtrG2PwAow)
+DB_CHANNEL_ID = os.environ.get("-1001911775310")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-PROTECT_CONTENT = True
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+OWNER_ID = int(os.environ.get("5591954930"))
+PROTECT_CONTENT = False
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1001721659524')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
