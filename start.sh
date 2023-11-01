f49f5c6687be51ce50d5df0b9def875b185de91b
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-11-02 04:30:08 +0530

    Update and rename start to start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Output for debugging purposes
+echo "Bot container started."
+
+# Pull the latest code from the Git repository
+git pull -f -q
+
+# Install Python dependencies from requirements.txt
+pip install --quiet -r requirements.txt
+
+# Start your bot using uvicorn and python3
+uvicorn api:app --host=0.0.0.0 --port=${PORT:-5000} & python3 -m bot
