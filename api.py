83afe52cc2469269484a757c077139b5f9d73ff3
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-11-02 04:55:27 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,5 @@
-from fastapi import FastAPI, Request
-
 app = FastAPI()
 
 @app.get('/')
-def health_check(request: Request):
-    client_host = request.client.host
-    server_host = request.url.hostname
-
-    response_data = {
-        "status": "ok",
-        "client_host": client_host,
-        "server_host": server_host,
-        "message": "Service is running smoothly."
-    }
-
-    return response_data
+def root(request: Request):
+    return {"status": "ok", "root": request.url.hostname}
