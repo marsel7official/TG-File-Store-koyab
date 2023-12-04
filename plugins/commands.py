9e431b6287e3a27839c1bc45a140bef7363a9d5f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-12-04 22:13:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -124,6 +124,60 @@ async def start(c, m, cb=False):
         )
 
 
+BATCH=[]
+
+@Client.on_message(filters.command('batch') & filters.private & filters.incoming)
+async def batch(c, m):
+    global IS_BATCH_PROCESSING
+    """ This is for batch command"""
+    if IS_PRIVATE:
+        if m.from_user.id not in AUTH_USERS:
+            return
+    IS_BATCH_PROCESSING = True
+    BATCH.append(m.from_user.id)
+    files = []
+    i = 1
+
+    while m.from_user.id in BATCH:
+        if i == 1:
+            media = await c.ask(chat_id=m.from_user.id, text='Send me some files or videos or photos or text or audio, if possible send 1 by 1. If you want to cancel the process send /cancel')
+            if media.text == "/cancel":
+                IS_BATCH_PROCESSING = False
+                return await m.reply_text('Cancelled Successfully ✌')
+            files.append(media)
+        else:
+            try:
+                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Done ✅', callback_data='done')]])
+                media = await c.ask(chat_id=m.from_user.id, text='Ok 😉. Now send me some more files Or press done to get shareable link. If you want to cancel the process send /cancel', reply_markup=reply_markup)
+                if media.text == "/cancel":
+                    IS_BATCH_PROCESSING = False
+                    return await m.reply_text('Cancelled Successfully ✌')
+                files.append(media)
+            except Exception as e:
+                print(e)
+        i += 1
+        
+    message = await m.reply_text("Generating shareable link 🔗")
+    string = ""
+    for file in files:
+        if DB_CHANNEL_ID:
+            copy_message = await file.copy(int(DB_CHANNEL_ID))
+        else:
+            copy_message = await file.copy(m.from_user.id)
+        string += f"{copy_message.id}-"
+        await asyncio.sleep(1)
+
+    string_base64 = await encode_string(string[:-1])
+    send = await c.send_message(m.from_user.id, string_base64) if not DB_CHANNEL_ID else await c.send_message(int(DB_CHANNEL_ID), string_base64)
+    base64_string1 = await encode_string(f"batch_{m.chat.id}_{send.id}")
+    base64_string2 = await encode_string(f"protectedbatch_{m.chat.id}_{send.id}")
+    bot = await c.get_me()
+    url1 = f"https://t.me/{bot.username}?start={base64_string1}"
+    url2 = f"https://t.me/{bot.username}?start={base64_string2}"
+
+    await message.edit(text=f"🔗 Normal Url: {url1}\n🛡️ Protected Url: {url2}")
+    IS_BATCH_PROCESSING = False
+
 @Client.on_message(filters.command('me') & filters.incoming & filters.private)
 async def me(c, m):
     """ This will be sent when /me command was used"""
