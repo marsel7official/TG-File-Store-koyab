ff23cff5d5060db666244a375664e637aa1f2e9d
MarselDeveloper <141441864+marsel7official@users.noreply.github.com>
2024-08-04 07:40:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 echo "Bot container started."
 git pull
 pip install --quiet -r requirements.txt
-uvicorn api:app --host=0.0.0.0 --port=${PORT:-8080} & python3 -m bot
+uvicorn api:app --host=0.0.0.0 --port=${PORT:-8000} & python3 -m bot
